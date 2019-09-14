13d66e8193a9864077f8ee65bb41e16fa7171403
mjalowiczor <m.jalowiczor@hotmail.com>
2019-09-14 18:44:34 +0200

    Add function generateLinks

--- js/script.js
+++ js/script.js
@@ -36,8 +36,75 @@ const titleClickHandler = function(event) {
   activeArticle.classList.add("active");
 };
 
-const links = document.querySelectorAll(".titles a");
+const generateTitleLinks = function(customSelector = "") {
+  const posts = document.querySelectorAll(".post" + customSelector);
+  console.log(customSelector);
+  let html = "";
+  for (let post of posts) {
+    const element =
+      "<li><a href=#" +
+      post.getAttribute("id") +
+      ">" +
+      post.querySelector(".post-title").innerText +
+      "</li></a>";
+    html += element;
+    console.log(html);
+  }
 
-for (let link of links) {
-  link.addEventListener("click", titleClickHandler);
+  document.querySelector(".list.titles").innerHTML = html;
+
+  /*Old version*/
+  //   const author = clickedElement;
+  //   const articles = document.querySelectorAll(".titles a");
+  //   const posts = document.querySelectorAll("article.post");
+  //   const backButton = document.getElementById("Back");
+  //   backButton.addEventListener("click", function() {
+  //     for (let article of articles) {
+  //       article.classList.remove("post");
+  //     }
+  //     backButton.classList.remove("active");
+  //     backButton.classList.add("post");
+  //   });
+
+  //   for (let article of articles) {
+  //     article.classList.add("post");
+  //     article.classList.remove("active");
+  //   }
+  //   for (const post of posts) {
+  //     let postAuthor = post
+  //       .querySelector(".post-author")
+  //       .innerHTML.replace("by ", "");
+  //     if (author.innerHTML == postAuthor) {
+  //       const postId = post.getAttribute("id");
+  //       for (let article of articles) {
+  //         const articlesHref = article.getAttribute("href");
+  //         if (articlesHref.replace("#", "") == postId) {
+  //           article.classList.remove("post");
+  //           backButton.classList.remove("post");
+  //         }
+  //       }
+  //     }
+  //   }
+  const links = document.querySelectorAll(".titles a");
+
+  for (let link of links) {
+    link.addEventListener("click", titleClickHandler);
+  }
+};
+
+generateTitleLinks();
+const authors = document.querySelectorAll(".list.authors a");
+
+for (const author of authors) {
+  author.addEventListener("click", function() {
+    generateTitleLinks("[data-author='" + this.getAttribute("href") + "']");
+    document.querySelector("button").classList.remove("button--hide");
+    console.log(this, this.getAttribute("href"));
+    event.preventDefault();
+  });
 }
+
+document.querySelector("button").addEventListener("click", function() {
+  generateTitleLinks();
+  document.querySelector("button").classList.add("button--hide");
+});
